1a592f9eb1a0abf6fcc25beb1fe8d74aef6e7c35
Claudiars20 <Claudiars20@users.noreply.github.com>
2021-08-21 18:32:22 -0500

    frontend docente

---
+++ Segundo Entregable/Implementación/R.2. Registrar docentes/Frontend/src/App.js
@@ -0,0 +1,40 @@
+import { Route, Switch, useLocation } from "react-router";
+import Sidebar from "./Sidebar";
+import Docentes from "./Pages/Registro Docentes";
+import Team from "./Pages/Team";
+import Calender from "./Pages/Calender";
+import Documents from "./Pages/Documents";
+import Projects from "./Pages/Projects";
+import styled from "styled-components";
+import { AnimatePresence } from "framer-motion";
+
+const Pages = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #F3F2F2;
+`;
+
+function App() {
+  const location = useLocation();
+  return (
+    <>
+      <Sidebar />
+      <Pages>
+        <AnimatePresence exitBeforeEnter>
+          <Switch location={location} key={location.pathname}>
+            <Route exact path="/" component={Docentes} />
+            <Route path="/team" component={Team} />
+            <Route path="/calender" component={Calender} />
+            <Route path="/documents" component={Documents} />
+            <Route path="/projects" component={Projects} />
+          </Switch>
+        </AnimatePresence>
+      </Pages>
+    </>
+  );
+}
+
+export default App;
